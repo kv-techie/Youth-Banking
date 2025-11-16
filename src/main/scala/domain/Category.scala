a7ca2e811243e2a7cdde908b06219308bc732476
Kedhar Vinod <vinod.kedhar05@gmail.com>
2025-11-17 01:03:19 +0530

    Update Category.scala

--- src/main/scala/domain/Category.scala
+++ src/main/scala/domain/Category.scala
@@ -1,10 +1,23 @@
 package domain
 
-/** Merchant / transaction categories */
+/** Transaction categories for spending classification and limits */
 enum Category:
-  case Food, Travel, Education, Entertainment, Medical, Utilities, Grocery, Gifts, Crypto, Gambling, Other
-
-/** Purpose tag that a parent may assign to sent money */
-enum PurposeTag:
-  case Medical, Travel, Emergency, Education, ExamFees, Misc
-
+  case Food
+  case Transport       // FIXED: Added this missing category
+  case Travel
+  case Education
+  case Entertainment
+  case Shopping
+  case Medical
+  case Gifts
+  case Utilities
+  case Savings
+  case Investment
+  case Insurance
+  case Clothing
+  case PersonalCare
+  case Books
+  case Technology
+  case Other
+  case Crypto          // High-risk category
+  case Gambling        // High-risk category
